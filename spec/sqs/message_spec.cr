2f11cd7023c9cec287a8b796a4fb31605269bdff
Jamie Gaskins <jgaskins@hey.com>
2026-09-17 19:24:56 -0400

    Improve SQS message parsing

--- spec/sqs/message_spec.cr
+++ spec/sqs/message_spec.cr
@@ -4,23 +4,71 @@ require "../../src/sqs/message"
 module AWS
   module SQS
     describe Message do
-      it "parses its XML representation" do
-        id = UUID.random
-        xml = <<-XML
-        <Message>
-          <MessageId>#{id}</MessageId>
-          <ReceiptHandle>receipt_handle</ReceiptHandle>
-          <MD5OfBody>md5_of_body</MD5OfBody>
-          <Body>body!</Body>
-        </Message>
+      id = UUID.random
+      body = <<-XML
+        <MessageId>#{id}</MessageId>
+        <ReceiptHandle>receipt_handle</ReceiptHandle>
+        <MD5OfBody>md5_of_body</MD5OfBody>
+        <Body>body!</Body>
+        <Attribute><Name>SenderId</Name><Value>AIDAEXAMPLE</Value></Attribute>
+        <Attribute><Name>ApproximateReceiveCount</Name><Value>2</Value></Attribute>
+        <MessageAttribute>
+          <Name>label</Name>
+          <Value><DataType>String</DataType><StringValue>hello</StringValue></Value>
+        </MessageAttribute>
+        <MessageAttribute>
+          <Name>count</Name>
+          <Value><DataType>Number</DataType><StringValue>3</StringValue></Value>
+        </MessageAttribute>
+        <MessageAttribute>
+          <Name>ratio</Name>
+          <Value><DataType>Number</DataType><StringValue>0.5</StringValue></Value>
+        </MessageAttribute>
+        <MessageAttribute>
+          <Name>blob</Name>
+          <Value><DataType>Binary</DataType><BinaryValue>#{Base64.strict_encode("raw bytes")}</BinaryValue></Value>
+        </MessageAttribute>
         XML
 
-        message = Message.from_xml(xml)
+      it "parses its XML representation" do
+        message = Message.from_xml("<Message>#{body}</Message>")
 
         message.id.should eq id
         message.receipt_handle.should eq "receipt_handle"
         message.md5.should eq "md5_of_body"
         message.body.should eq "body!"
+        message.attributes.should eq({
+          "SenderId"                => "AIDAEXAMPLE",
+          "ApproximateReceiveCount" => "2",
+        })
+        message.message_attributes["label"].should eq "hello"
+        message.message_attributes["count"].should eq 3_i64
+        message.message_attributes["ratio"].should eq 0.5
+        message.message_attributes["blob"].should eq "raw bytes".to_slice
+      end
+
+      it "parses XML that declares the SQS namespace" do
+        xml = %(<Message xmlns="http://queue.amazonaws.com/doc/2012-11-05/">#{body}</Message>)
+
+        message = Message.from_xml(xml)
+
+        message.id.should eq id
+        message.attributes["SenderId"].should eq "AIDAEXAMPLE"
+        message.message_attributes["count"].should eq 3_i64
+      end
+
+      it "leaves attributes empty when the message has none" do
+        message = Message.from_xml(<<-XML)
+          <Message>
+            <MessageId>#{id}</MessageId>
+            <ReceiptHandle>rh</ReceiptHandle>
+            <MD5OfBody>md5</MD5OfBody>
+            <Body>b</Body>
+          </Message>
+          XML
+
+        message.attributes.should be_empty
+        message.message_attributes.should be_empty
       end
     end
   end
--- src/sqs/message.cr
+++ src/sqs/message.cr
@@ -1,10 +1,14 @@
+require "base64"
 require "xml"
 require "uuid"
 
 module AWS
   module SQS
+    class InvalidXML < ::Exception
+    end
+
     struct Message
-      getter id, receipt_handle, md5, body
+      getter id, receipt_handle, md5, body, attributes, message_attributes
 
       def self.from_xml(xml : String)
         from_xml XML.parse(xml).first_element_child.not_nil!
@@ -33,11 +37,8 @@ module AWS
 
       private def self.get_attributes(xml : XML::Node)
         attributes = {} of String => String
-        xml.xpath_nodes("xmlns:Attribute").each do |attribute|
-          name_node = attribute.xpath_node("./xmlns:Name").not_nil!
-          value_node = attribute.xpath_node("./xmlns:Value").not_nil!
-
-          attributes[name_node.text] = value_node.text
+        children(xml, "Attribute").each do |attribute|
+          attributes[child(attribute, "Name").text] = child(attribute, "Value").text
         end
 
         attributes
@@ -45,34 +46,49 @@ module AWS
 
       private def self.get_message_attributes(xml : XML::Node)
         attributes = {} of String => String | Bytes | Int64 | Float64
-        xml.xpath_nodes("xmlns:MessageAttribute").each do |attribute|
-          name_node = attribute.xpath_node("./xmlns:Name").not_nil!
-          value_node = attribute.xpath_node("./xmlns:Value").not_nil!
-          value_type = value_node.xpath_node("./xmlns:DataType").not_nil!
-          value = case value_type.text
+        children(xml, "MessageAttribute").each do |attribute|
+          name = child(attribute, "Name").text
+          value_node = child(attribute, "Value")
+          value = case child(value_node, "DataType").text
                   when "String"
-                    value_node.xpath_node("./xmlns:StringValue").not_nil!.text
+                    child(value_node, "StringValue").text
                   when "Number"
-                    string = value_node.xpath_node("./xmlns:StringValue").not_nil!.text
+                    string = child(value_node, "StringValue").text
                     if string.includes? '.'
                       string.to_f64
                     else
                       string.to_i64
                     end
                   when "Binary"
-                    Base64.decode(value_node.xpath_node("./xmlns:BinaryValue").not_nil!.text)
+                    Base64.decode(child(value_node, "BinaryValue").text)
                   else # Return bytes for custom data types in case it's not UTF8 strings
-                    value_node.xpath_node("./xmlns:BinaryValue").not_nil!.text.to_slice
+                    child(value_node, "BinaryValue").text.to_slice
                   end
 
-          attributes[name_node.text] = value
+          attributes[name] = value
         end
 
         attributes
       end
 
       private def self.get_xml_child(xml, name) : String
-        xml.xpath_node("*[name()='#{name}']/text()").to_s
+        child?(xml, name).try(&.text) || ""
+      end
+
+      # SQS responses declare a default namespace on the root element, which
+      # makes the `xmlns:` XPath prefix work, but fragments and some
+      # compatible services omit it. Matching on the local name works either
+      # way.
+      private def self.children(xml : XML::Node, name : String)
+        xml.xpath_nodes("*[local-name()='#{name}']")
+      end
+
+      private def self.child?(xml : XML::Node, name : String) : XML::Node?
+        xml.xpath_node("*[local-name()='#{name}']")
+      end
+
+      private def self.child(xml : XML::Node, name : String) : XML::Node
+        child?(xml, name) || raise InvalidXML.new("Expected a <#{name}> element inside: #{xml}")
       end
     end
   end
